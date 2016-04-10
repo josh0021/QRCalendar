c0f53bb6d919d649f6e141e3e0f4b687e233b78f
surfguy0021 <surfguy0021@gmail.com>
2016-04-09 21:56:46 -0500

    First commit

---
+++ QRCalendar/App.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using EventKit;
+	public class App
+	{
+		public static App Current {
+			get { return current; }
+		}
+		private static App current;
+
+		public EKEventStore EventStore {
+			get { return eventStore; }
+		}
+		protected EKEventStore eventStore;
+
+		static App ()
+		{
+			current = new App();
+		}
+		protected App () 
+		{
+			eventStore = new EKEventStore ( );
+		}
+	}
+
---
+++ QRCalendar/QRToCalendar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+namespace QRCalendar
+{
+	public static class CalendarFileExtentions
+	{
+		public static DateTime DateToDateTime(string s){
+			string ical = s;
+			char[] delim = { '\n' };
+			string[] lines = ical.Split(delim);
+			delim[0] = ':';
+			for (int i = 0; i < lines.Length; i++)
+			{
+				if (lines[i].Contains("BEGIN:VEVENT"))
+				{
+					string[] eventData = new string[9];
+					for (int j = 0; j < 9; j++)
+						eventData[j] = lines[i + j + 1].Split(delim)[1];
+					string strDate = eventData[0].ToString();
+					strDate = strDate.Replace("\r", "");
+
+					string format;
+					DateTime result;
+					CultureInfo provider = CultureInfo.InvariantCulture;
+					format = "yyyyMMddThhmmssssZ";
+					result = DateTime.ParseExact(strDate, format, provider);
+					i += 10;
+				}
+			}
+		}
+
+	}
+}
+
